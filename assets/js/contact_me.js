7e803ef77d5d9f813aa0747412eed4858fc483ee
Raphael Ochsenbein <raphael@akehir.com>
2016-03-25 15:12:17 +0100

    inserted prices for tours

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -10,20 +10,28 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
+            var time = $("input#time").val();
+            var people = $("input#people").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
+            console.log(name);
+            console.log(email);
+            console.log(time);
+            console.log(people);
+            console.log(message);
+
             $.ajax({
                 url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone,
                     email: email,
+                    time: time,
+                    people: people,
                     message: message
                 },
                 cache: false,
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,28 @@
 <?php
 // check if fields passed are empty
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
+if( empty($_POST['name'])  		||
+    empty($_POST['email']) 		||
+    empty($_POST['time']) 		||
+    empty($_POST['people']) 	||
+    empty($_POST['message'])	||
+    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+    {
+	 echo "No arguments Provided!";
+	 return false;
+    }
 	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-$phone = $_POST['phone'];
+$time = $_POST['time'];
+$people = $_POST['people'];
 $message = $_POST['message'];
 	
 // create email body and send it	
-$to = 'info@travelforchangeafrica.org'; // *REPLACE WITH THE EMAIL ADDRESS YOU WANT THE FORM TO SEND MAIL TO*
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$to = 'info@travelforchangeafrica.org';//'info@travelforchangeafrica.org'; // *REPLACE WITH THE EMAIL ADDRESS YOU WANT THE FORM TO SEND MAIL TO*
+$email_subject = "Travel For Change Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nDates of Stay: $time\n\nNumber of Peoples: $people\n\nMessage:\n$message";
 $headers = "From: noreply@travelforchangeafrica.org\n"; // *REPLACE WITH THE EMAIL ADDRESS YOU WANT THE MESSAGE TO BE FROM*
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+return true;
 ?>
