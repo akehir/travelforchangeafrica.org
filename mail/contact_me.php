17315daf2743b291d95919eac5dee9ed06aeaa34
Raphael Ochsenbein <raphael@akehir.com>
2016-03-26 12:16:30 +0100

    small bugfixes to first page version

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,9 +18,9 @@ $people = $_POST['people'];
 $message = $_POST['message'];
 	
 // create email body and send it	
-$to = 'info@travelforchangeafrica.org';//'info@travelforchangeafrica.org'; // *REPLACE WITH THE EMAIL ADDRESS YOU WANT THE FORM TO SEND MAIL TO*
-$email_subject = "Travel For Change Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nDates of Stay: $time\n\nNumber of Peoples: $people\n\nMessage:\n$message";
+$to = 'info@akehir.com';//'info@travelforchangeafrica.org'; // *REPLACE WITH THE EMAIL ADDRESS YOU WANT THE FORM TO SEND MAIL TO*
+$email_subject = "Travel For Change Booking for:  $email_address";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nDuration: $time\n\nNr of People: $people\n\nMessage:\n$message";
 $headers = "From: noreply@travelforchangeafrica.org\n"; // *REPLACE WITH THE EMAIL ADDRESS YOU WANT THE MESSAGE TO BE FROM*
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
